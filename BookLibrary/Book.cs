ede83259cce5f5a5b1723e303ce34e5b379b5e29
Charlie Philpott <Philpott_001@hotmail.com>
2017-10-17 21:51:58 +0100

    Add project files.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookLibrary
+{
+    class Book
+    {
+        //=== Class Variables ===/
+        public static string projectPath = Directory.GetCurrentDirectory();
+        public static string libraryPath = projectPath + @"\book library\";
+
+
+        //=== Class Methods ===/
+        public string BookName { get; set; }
+
+        public string BookAuthor { get; set; }
+
+        public string DatePublished { get; set; }
+
+        public string Price { get; set; }
+
+        // Create new Library
+        public void CreateNewBook(Book aBook)
+        {
+            try
+            {
+                // Creates a new txt file within the "book libraries" folder
+                FileStream newFile = File.Create(libraryPath + aBook.BookName + ".txt");
+                newFile.Dispose();
+                
+                // Success message
+                Console.WriteLine("You have added " + aBook.BookName + " to the library.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+
+        // Add information to the book
+        public void AddInformationToBook(Book aBook)
+        {
+            try
+            {
+                // Define the Author of the book
+                Console.WriteLine("Who is the author?");
+                aBook.BookAuthor = "Author: " + Console.ReadLine();
+
+                // Define the date when the book was published 
+                Console.WriteLine("What date was the book originally published?");
+                aBook.DatePublished = "Date published: " + Console.ReadLine();
+
+                // Define the price of the book
+                Console.WriteLine("What is the price of the book?");
+                aBook.Price = "Price: " + Console.ReadLine();
+
+                // Add all the inforation to an array varibale
+                List<string> bookInformation = new List<string>();
+                bookInformation.Add(aBook.BookAuthor);
+                bookInformation.Add(aBook.DatePublished);
+                bookInformation.Add(aBook.Price);
+
+                // Defines the folder book path
+                string bookPath = libraryPath + aBook.BookName + ".txt";
+
+                // Write the information to the book file
+                File.WriteAllLines(bookPath, bookInformation);                
+
+                // Success message
+                Console.WriteLine("You have successfully addded information to " + aBook.BookName);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+
+        // Read the books information
+        public void UpdateInformationInBook(Book aBook)
+        {
+
+            try
+            {
+                // Detects all the information within the book
+                string[] informationLines = File.ReadAllLines(libraryPath + aBook.BookName + ".txt");
+
+                List<string> informationList = new List<string>();
+                string[] informationValueSplit;
+                Char splitDelimiter = ':';
+
+                foreach (string informationLine in informationLines) {
+                    informationValueSplit = informationLine.Split(splitDelimiter);
+                    informationList.Add(informationValueSplit[1].TrimStart());
+                }
+
+                aBook.BookAuthor = informationList[1];
+                aBook.DatePublished = informationList[2];
+                aBook.Price = informationList[3];
+
+                aBook.displayAllInformation(aBook);
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+
+        public void displayAllInformation(Book aBook) {
+            Console.WriteLine("Name: " + aBook.BookName);
+            Console.WriteLine("Author: " + aBook.BookAuthor);
+            Console.WriteLine("Date Published: " + aBook.DatePublished);
+            Console.WriteLine("Price: " + aBook.Price);            
+        }
+
+
+    }
+}
---
+++ BookLibrary/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace BookLibrary
+{
+    class Program
+    {
+        //=== Class Variables ===/
+        public static Book CurrentBook;
+        public static string projectPath = Directory.GetCurrentDirectory();
+        public static string libraryPath = projectPath + @"\book library\";
+
+
+        static void Main(string[] args)
+        {
+
+            // Set Users name
+            //Console.WriteLine("Hello, what is your name?");
+            //User User1 = new User();
+            //User1.UserName = Console.ReadLine();
+            //Console.WriteLine("Hi " + User1.UserName);
+
+            //=== Add a new book to the library
+            //string createNewBookQuestion = "Do you want to add a new book to the library (Yes / No)?";
+            //string createNewBookAnswer = inputCheck(createNewBookQuestion);
+            //if (createNewBookAnswer == "Yes")
+            //{
+            //    Console.WriteLine("What is the title of the new book?");
+            //    string newBook = Console.ReadLine();
+
+            //    // Creates the new book
+            //    Book Book1 = new Book();
+            //    setCurrentBook(Book1);
+            //    Book1.BookName  = newBook;
+            //    Book1.CreateNewBook(Book1);
+
+            //    // Adds information to the newly created book
+            //    string accessLibraryQuestion = "Do you want to add some information to the new book (Yes / No)?";
+            //    string accessLibraryAnswer = inputCheck(accessLibraryQuestion);
+            //    if (accessLibraryAnswer == "Yes")
+            //    {
+            //        Book1.AddInformationToBook(Book1);
+            //    }
+            //    else if (accessLibraryAnswer == "No")
+            //    {
+            //        Console.WriteLine("Ok");
+            //    }
+            //}
+            //else if (createNewBookAnswer == "No")
+            //{
+            //    Console.WriteLine("Ok");
+            //}
+
+            // Update information within the library
+            string readLibraryQuestion = "Do you want to upadte any information within the library (Yes / No)?";
+            string readLibraryAnswer = inputCheck(readLibraryQuestion);
+            if (readLibraryAnswer == "Yes")
+            {
+                readLibraryFileNames();
+
+                string bookToUpdateQuestion = "Which book from the above list do you want to update?";
+                string bookToUpdateAnswer = checkBookExists(bookToUpdateQuestion);
+
+                Book bookToUpdate = new Book();
+                setCurrentBook(bookToUpdate);
+                bookToUpdate.BookName = bookToUpdateAnswer;
+                bookToUpdate.UpdateInformationInBook(bookToUpdate);
+
+
+            }
+            else if (readLibraryAnswer == "No")
+            {
+                Console.WriteLine("TOk");
+                Console.ReadLine();
+            }
+        }
+
+
+        // ==== Class Methods ===/
+        public Book getCurrentBook() {
+            return CurrentBook;
+        }
+        private static void setCurrentBook(Book aBook) {
+            CurrentBook = aBook;
+        }
+
+        // Checks if the user input is correct 
+        static public string inputCheck(string aQuestion)
+        {
+            bool incorrectInput = true;
+            while (incorrectInput)
+            {
+                Console.WriteLine(aQuestion);
+                string input = Console.ReadLine();
+
+                if (input == "Yes" || input == "No")
+                {
+                    incorrectInput = false;
+
+                    if (input == "Yes") {
+                        return input;
+                    }
+                    else if (input == "No") {
+                        return input;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, I did not understand that.");
+                }
+            }
+            return "";
+        }                
+
+        // Discovered and dispays all the books within the library
+        static private void readLibraryFileNames()
+        {
+            // Detects all the books within the library
+            string[] fileNames = Directory.GetFiles(libraryPath);
+
+            // Iterates over all the books within the library
+            foreach (string fileName in fileNames)
+            {
+                // Displays the file name
+                Console.WriteLine(Path.GetFileNameWithoutExtension(fileName));
+            }
+        }
+
+        // Checks if the book exsists
+        static private string checkBookExists(string aQuestion)
+        {
+
+            bool bookExsists = false;
+            bool whileLoop = true;
+            string bookName = "";
+
+            // Detects all the books within the library
+            string[] fileNames = Directory.GetFiles(libraryPath);
+
+            while (whileLoop)
+            {
+                Console.WriteLine(aQuestion);
+                bookName = Console.ReadLine();
+
+                // Iterates over the books within the library
+                foreach (string fileName in fileNames)
+                {
+                    if (Path.GetFileNameWithoutExtension(fileName) == bookName)
+                    {
+                        bookExsists = true;
+                        whileLoop = false;
+                    }
+                }
+
+                // If the book does not exsist
+                if (!bookExsists)
+                {
+                    // Unsuccessful message
+                    Console.WriteLine(bookName + " was not found within the library.");
+                }
+            }
+
+            return bookName;
+        }
+    }
+}
